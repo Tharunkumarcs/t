5bcb564d1d3c734be70cacb89fca62eb132c08f6
Tharunkumarcs <47142208+Tharunkumarcs@users.noreply.github.com>
2019-02-11 15:45:52 +0530

    Create sign.c

---
+++ sign.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("\n the number is positive");
+}
+else if(a<0)
+{
+printf("\n the number is negative)
+}
+else
+{
+printf("\n the number is zero");
+}
+getch();
+}
